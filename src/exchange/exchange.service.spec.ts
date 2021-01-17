804d36b7aeede07098da5449adeeb083a292a151
abimaelandrade <abimaelc.andrade@gmail.com>
2021-01-17 10:09:53 -0300

    ensure called getCurrency twice

--- src/exchange/exchange.service.spec.ts
+++ src/exchange/exchange.service.spec.ts
@@ -1,16 +1,25 @@
 import { BadRequestException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import { ExchangeService } from './exchange.service';
+import { CurrenciesService, ExchangeService } from './exchange.service';
 
 describe('ExchangeService', () => {
   let service: ExchangeService;
+  let currenciesService: CurrenciesService;
 
   beforeEach(async () => {
+    const currencyServiceMock = {
+      getCurrency: jest.fn()
+    }
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ExchangeService],
+      providers: [
+        ExchangeService, 
+        { provide: CurrenciesService, useFactory: () => currencyServiceMock }
+      ],
     }).compile();
 
     service = module.get<ExchangeService>(ExchangeService);
+    currenciesService = module.get<CurrenciesService>(CurrenciesService);
   });
 
   it('should be defined', () => {
@@ -36,6 +45,12 @@ describe('ExchangeService', () => {
           amount: 1
         }),
       ).resolves.not.toThrow();
+     });
+
+     it('should be called getCurrency twice', async () => {
+       await service.convertAmount({ from: 'USD', to: 'BRL', amount: 1 });
+      
+      await expect(currenciesService.getCurrency).toBeCalledTimes(2);
      });
   });
 
--- src/exchange/exchange.service.ts
+++ src/exchange/exchange.service.ts
@@ -1,10 +1,17 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
+export class CurrenciesService {
+  async getCurrency(currency: string): Promise<any> {}
+}
 
 @Injectable()
 export class ExchangeService {
+  constructor(private currencyService: CurrenciesService) { }
   async convertAmount({from, to, amount}) : Promise<any> {
     if(!from || !to || !amount){
       throw new BadRequestException()
     }
+
+    const currencyFrom = this.currencyService.getCurrency(from);
+    const currencyTo = this.currencyService.getCurrency(to);
   }
 }
